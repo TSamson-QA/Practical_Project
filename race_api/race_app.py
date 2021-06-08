9da754d10bc18b94cc9f6a33e28e92826b5abc77
Tom-Samson <TSamson@qa.com>
2021-06-08 12:42:05 +0100

    added service 3

--- race_api/race_app.py
+++ race_api/race_app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route('/get_race')
 def get_race():
+    #randomly gets values from set list
     return random.choice({1:'Dwarf', 2:'Halfling', 3:'Elf', 4:'Gnome', 5:'Human', 6:'Half-Elf', 7:'Tiefling', 8:'Dragonborn', 9:'Half-Orc'})
 
 if __name__ == "__main__":
