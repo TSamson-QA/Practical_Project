7ce797fab7fbba863cb8c52c8d0cf330fba91762
Tom-Samson <TSamson@qa.com>
2021-06-08 16:42:47 +0100

    fixed port allocation error

--- race_api/race_app.py
+++ race_api/race_app.py
@@ -9,4 +9,4 @@ def get_race():
     return random.choice({1:'Dwarf', 2:'Halfling', 3:'Elf', 4:'Gnome', 5:'Human', 6:'Half-Elf', 7:'Tiefling', 8:'Dragonborn', 9:'Half-Orc'})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
