5dfc0900603974d4d98a7d4b07c47fd4c33a65f6
Tom-Samson <TSamson@qa.com>
2021-06-10 18:01:38 +0100

    editing testing

--- server/tests/test_server.py
+++ server/tests/test_server.py
@@ -1,7 +1,7 @@
 from flask import url_for
 from flask_testing import TestCase
 import requests_mock
-from unittest.mock import patch
+
 
 from app import app
 
@@ -10,9 +10,10 @@ class TestBase(TestCase):
         return app
 
 class TestHome(TestBase):
-    def test_get_class(self):
-        with patch('random.choice') as test_class:
-            test_class.return_value = 'Human'
+    def test_home(self):
+        with requests_mock.Mocker() as mocker:
+            mocker.get('http://class_api:5001/get_class', text='Druid')
+            mocker.get('http://race_api:5002/get_race', text='Human')
             response = self.client.get(url_for('home'))
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(b'Human', response.data)
+            self.assertIn(b'The generated character is a Human Druid with an alignment of True Neutral', response.data)
