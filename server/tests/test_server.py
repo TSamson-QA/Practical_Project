bf258fac3bbc4cb583e7f9ec4302c64f71964490
Tom-Samson <TSamson@qa.com>
2021-06-10 17:42:38 +0100

    editing testing

--- server/tests/test_server.py
+++ server/tests/test_server.py
@@ -3,6 +3,7 @@ from flask_testing import TestCase
 import requests_mock
 
 from app import app
+from app import class_, race, gen_alignment
 
 class TestBase(TestCase):
     def create_app(self):
@@ -11,8 +12,8 @@ class TestBase(TestCase):
 class TestHome(TestBase):
     def test_home(self):
         with requests_mock.Mocker() as mocker:
-            mocker.get('http://class_api:5001/get_class', text='Cleric')
-            mocker.get('http://race_api:5002/get_race', text='Dwarf')
+            mocker.get('http://class_api:5001/get_class', text=class_)
+            mocker.get('http://race_api:5002/get_race', text=race)
             response = self.client.get(url_for('home'))
             self.assertEqual(response.status_code, 200)
-            self.assertIn(b'The generated character is a Dwarf Cleric with an alignment of Lawful Good', response.data)
+            self.assertIn(f'The generated character is a {race} {class_} with an alignment of {gen_alignment}', response.data)
