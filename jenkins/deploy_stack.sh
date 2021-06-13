11174f970ff3c84691b02efec4949c25dae3f7c1
Tom-Samson <TSamson@qa.com>
2021-06-14 00:22:25 +0100

    attempt fix

--- jenkins/deploy_stack.sh
+++ jenkins/deploy_stack.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+docker build -t alignment_api alignment_api 
+
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
     export DATABASE_URI=${DATABASE_URI}
