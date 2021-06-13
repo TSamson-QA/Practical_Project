08fbf804d976ee1fac804f21ad0a7d8730689855
Tom-Samson <TSamson@qa.com>
2021-06-13 21:23:18 +0100

    added testing to service 4

--- alignment_api/test_alignment_api.py
+++ alignment_api/test_alignment_api.py
@@ -10,5 +10,5 @@ class TestBase(TestCase):
 class TestHome(TestBase):
     def test_get_alignment(self):
         for _ in range(20):
-            response = self.client.get(url_for('get_class'))
+            response = self.client.get(url_for('get_alignment'))
             self.assertIn(response.data.decode("utf-8"),['Lawful Good','Neutral Good', 'Chaotic Good', 'Lawful Neutral', 'True Neutral', 'Chaotic Neutral', 'Lawful Evil', 'Neutral Evil', 'Chaotic Evil'])
