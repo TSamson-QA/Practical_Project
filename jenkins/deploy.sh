7e7018aeb186fa4d482586ad37b2caf7775d116c
Tom-Samson <TSamson@qa.com>
2021-06-14 14:11:44 +0100

    changed alignment

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -10,7 +10,7 @@ ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
     docker stack deploy --compose-file docker-compose.yaml character_generator
 EOF
 # Build Server
-docker build -t ${project_name}_server server
+docker build -t character_generator_server server
 
 # Build class_api
 docker build -t class_api class_api 
@@ -22,25 +22,22 @@ docker build -t race_api race_api
 docker build -t alignment_api alignment_api 
 
 # Create Network
-docker network create ${project_name}_network 
+docker network create character_generator_network 
 
 # Run containers
 docker run -d -p 5000:5000 \
-    --name ${project_name}_server \
-    --network ${project_name}_network \
-    ${project_name}_server
+    --name character_generator_server \
+    --network character_generator_network \
+    character_generator_server
 
 docker run -d \
     --name class_api \
-    --network ${project_name}_network \
+    --network character_generator_network \
     class_api
 
 docker run -d  \
     --name race_api \
-    --network ${project_name}_network \
+    --network character_generator_network \
     race_api
 
-docker run -d  \
-    --name alignment_api \
-    --network ${project_name}_network \
-    alignment_api
+docker run -d --name alignment_api --network character_generator_network alignment_api
